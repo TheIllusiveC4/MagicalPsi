99bc14de14b4eedea9ffea7297af69fc8e16ae21
TheIllusiveC4 <itwasmec4@gmail.com>
2020-09-24 23:00:26 -0700

    remove redundant armor fixes, closes #1

--- src/main/java/top/theillusivec4/magipsi/client/ClientMagicalPsi.java
+++ src/main/java/top/theillusivec4/magipsi/client/ClientMagicalPsi.java
@@ -15,31 +15,20 @@ package top.theillusivec4.magipsi.client;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.color.ItemColors;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.util.LazyValue;
 import top.theillusivec4.magipsi.MagicalPsi;
 import top.theillusivec4.magipsi.client.render.FocusingPlateModel;
-import top.theillusivec4.magipsi.client.render.SensorLayer;
 import vazkii.psi.api.spell.SpellParam;
 import vazkii.psi.common.item.ItemExosuitSensor;
 import vazkii.psi.common.item.armor.ItemPsimetalArmor;
-import vazkii.psi.common.item.armor.ItemPsimetalExosuitHelmet;
 import vazkii.psi.common.item.base.ModItems;
 
 public class ClientMagicalPsi {
 
   public static void setupPsiOverrides() {
-    // todo: Remove when/if Psi implements the PR fix
-    ItemColors colors = Minecraft.getInstance().getItemColors();
-    colors.register((stack, tintIndex) -> tintIndex < 1 ? -1
-            : ((ItemPsimetalExosuitHelmet) stack.getItem()).getColor(stack),
-        ModItems.psimetalExosuitHelmet);
-
     SpellParam.RED = 0xFF003F;
     SpellParam.GREEN = 0x3FFF00;
     SpellParam.BLUE = 0x007FFF;
@@ -69,13 +58,6 @@ public class ClientMagicalPsi {
     } catch (NoSuchFieldException | IllegalAccessException e) {
       MagicalPsi.LOGGER.error("Reflection error in models!");
     }
-  }
-
-  // todo: Remove when/if Psi implements the PR fix
-  public static void setupSensorLayer() {
-    EntityRendererManager rendererManager = Minecraft.getInstance().getRenderManager();
-    rendererManager.getSkinMap().values().forEach(renderer -> renderer
-        .addLayer(new SensorLayer<>(renderer, new BipedModel<>(0.5F), new BipedModel<>(1.0F))));
   }
 
   public static LazyValue<BipedModel<?>> getArmorModel(EquipmentSlotType slot) {
